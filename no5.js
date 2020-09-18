2edc336e44b01ab4707b15874a8eb12833093eec
Dzulfanni <belajarayok866@gmail.com>
2020-09-18 22:23:01 +0700

    first commit

---
+++ no5.js
@@ -0,0 +1,15 @@
+// santriProgrammer("Rahmad",status, "Medan") 
+
+santriProgrammer("rahmad", status, "medan") {
+    function ("rahmad", "medan")
+}
+
+function ()
+
+callback status() {
+    alert('status saya adalah santri')
+}
+
+status('rahmad', 'medan', status)
+
+alert('status saya dalah santri')
